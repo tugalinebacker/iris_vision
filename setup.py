4f756fba25859c5f0a71b01c63d76e7eab2fb96e
tugalinebacker <msilva.cmiguel@gmail.com>
2021-10-29 19:01:18 +0100

    Minor updates

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
  
 setup(
     version='0.0.0',
-    scripts=['src/inference_mkII.py', 'src/Doppler.py'],
+    scripts=['src/inference_mkII.py', 'src/sensor_nav.py'],
     packages=['iris_vision','object_detection'],
     package_dir={'': 'src'}
 )
