6d4dfb33ff1e239faacecd01789db9ce508ce7dc
tugalinebacker <msilva.cmiguel@gmail.com>
2023-11-09 19:42:29 +0000

    Rename inference_mkII to ghost_net_inference

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
  
 setup(
     version='0.0.0',
-    scripts=['src/inference_mkII.py', 'src/sensor_nav.py'],
+    scripts=['src/ghost_net_inference.py', 'src/sensor_nav.py'],
     packages=['iris_vision','object_detection'],
     package_dir={'': 'src'}
 )
