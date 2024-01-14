ca283d4e099cb02149231819b478bef10ef0b35f
tugalinebacker <msilva.cmiguel@gmail.com>
2024-01-14 13:42:05 +0000

    Minor changes.

--- src/ghost_net_inference.py
+++ src/ghost_net_inference.py
@@ -22,14 +22,15 @@ gpus = tf.config.experimental.list_physical_devices('GPU')
 for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
 
+efficientdet_d0 = 'exported-models/ghost_mkII_efficientdet_d0_GPU/'
+resnet50_CPU = 'exported-models/ghost_mkII_resnet50_CPU/'
+mobilenet_CPU = 'exported-models/ghost_mkII_mobilenet_CPU/'
 
 dirname = os.path.dirname(__file__)
-PATH_TO_MODEL_DIR = os.path.join(dirname, 'exported-models/ghost_mkII_efficientdet_d0_GPU/')
-# PATH_TO_MODEL_DIR = os.path.join(dirname, 'exported-models/ghost_mkII_resnet50_CPU/')
-# PATH_TO_MODEL_DIR = os.path.join(dirname, 'exported-models/ghost_mkII_mobilenet_CPU/')
+PATH_TO_MODEL_DIR = os.path.join(dirname, efficientdet_d0)
 PATH_TO_LABELS = os.path.join(dirname, 'annotations/label_map.pbtxt')
 MIN_CONF_THRESH = 0.6
-PATH_TO_SAVED_MODEL = os.path.join(dirname, 'exported-models/ghost_mkII_efficientdet_d0_GPU/saved_model')
+PATH_TO_SAVED_MODEL = os.path.join(dirname, efficientdet_d0 + 'saved_model')
 
 print('Loading model...', end='')
 start_time = time.time()
@@ -48,10 +49,9 @@ category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABE
 
 
 def callback(img):
-    #rospy.loginfo("Topic /iris/proscilica_front/image_color detected!")
     bridge = CvBridge()
-    ghost_detection = rospy.Publisher("/iris/ghost_detection", Image)
-    center_points = rospy.Publisher("/iris/image_center_points", Float32MultiArray)
+    ghost_detection = rospy.Publisher("/iris/proscilica_front/ghost_detection", Image)
+    center_points = rospy.Publisher("/iris/proscilica_front/image_center_points", Float32MultiArray)
 
     try:
         cv_image = bridge.imgmsg_to_cv2(img, "bgr8") #converts ROS image to cv2
@@ -59,7 +59,6 @@ def callback(img):
         print(e)
     
     cv_image = cv2.resize(cv_image, (640,640))
-    #(rows,cols,channels) = cv_image.shape
 
     input_tensor = tf.convert_to_tensor(cv_image)
     input_tensor = input_tensor[tf.newaxis, ...]
@@ -102,19 +101,20 @@ def callback(img):
     
     # VALUE OF THE BOUNDING BOX WITH THE HIGHEST SCORE
     highest_confidence_bbox_score = confidence_scores[max_confidence_index]
-    print("Highest confidence box score -> " + str(round(highest_confidence_bbox_score,2)))
+    print("\nHighest confidence box score -> " + str(round(highest_confidence_bbox_score,2)))
     
     # COORDINATES OF THE BOUNDING BOX WITH THE HIGHEST SCORE
     highest_confidence_bbox_coordinates = bounding_boxes[max_confidence_index]
     highest_confidence_bbox_coordinates_pixel = bounding_boxes_pixel[max_confidence_index]
-    print("Highest confidence box coordinates in pixels -> " + str(highest_confidence_bbox_coordinates_pixel))
 
-    # CENTER POINTS OF THE FRAME AND THE HIGHEST SCORING BOUNDING BOX
+    # COORDINATES OF THE CENTER POINTS OF THE FRAME AND HIGHEST SCORING BOUNDING BOX
     x_frame_center_point = image_width/2
     y_frame_center_point = image_height/2
     x_bbox_center_point = highest_confidence_bbox_coordinates_pixel[1]+highest_confidence_bbox_coordinates_pixel[3]/2
     y_bbox_center_point = highest_confidence_bbox_coordinates_pixel[0]+highest_confidence_bbox_coordinates_pixel[2]/2
+    print("Box center point coordinates -> ( " + str(round(x_bbox_center_point,2)) + ", " + str(round(y_bbox_center_point,2)) + ")")
     
+    # STORE COORDINATES IN ROS MESSAGE
     center_points_coordinates = Float32MultiArray()
     center_points_coordinates.data = [x_frame_center_point, y_frame_center_point, x_bbox_center_point, y_bbox_center_point]
 
